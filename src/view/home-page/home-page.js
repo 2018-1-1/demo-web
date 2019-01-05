b632939836efa9b7ebd73082ae40b6e310384090
Merlynr <lcq1013962426@gmail.com>
2019-01-06 00:24:02 +0800

    主页面Loading....

--- src/view/home-page/home-page.js
+++ src/view/home-page/home-page.js
@@ -1,10 +1,25 @@
 export const homePage={
     data () {
         return {
-            
+            // isCollapse:false
+            centerDialogVisible: false
         }
     },
     methods: {
+        popover(){
+            if(this.centerDialogVisible==false){
+                this.centerDialogVisible=true
+            }else{
+                this.centerDialogVisible=false
+            }
+        },
+        pageActions(){
+            if(this.isCollapse==true){
+                this.isCollapse=false
+            }else{
+                this.isCollapse=true
+            }
+        },
         handleOpen(key, keyPath) {
             console.log(key, keyPath);
           },
--- src/view/home-page/home-page.vue
+++ src/view/home-page/home-page.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <el-container>
-      <el-header>Header</el-header>
-      <el-main>
-        <el-menu default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+      <el-aside width="200px">
+        <el-menu default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+          background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" style="height:100%">
+          <el-menu-item index="0" style="background:#22a7f0">
+            <i class="el-icon-menu"></i>
+            <span>人才培养管理系统</span>
+          </el-menu-item>
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span slot="title">导航一</span>
+              <span>导航一</span>
             </template>
             <el-menu-item-group>
-              <span slot="title">分组一</span>
+              <template slot="title">分组一</template>
               <el-menu-item index="1-1">选项1</el-menu-item>
               <el-menu-item index="1-2">选项2</el-menu-item>
             </el-menu-item-group>
@@ -18,7 +22,7 @@
               <el-menu-item index="1-3">选项3</el-menu-item>
             </el-menu-item-group>
             <el-submenu index="1-4">
-              <span slot="title">选项4</span>
+              <template slot="title">选项4</template>
               <el-menu-item index="1-4-1">选项1</el-menu-item>
             </el-submenu>
           </el-submenu>
@@ -35,8 +39,30 @@
             <span slot="title">导航四</span>
           </el-menu-item>
         </el-menu>
-      </el-main>
-      <el-footer>Footer</el-footer>
+      </el-aside>
+      <el-container style="position:relative;top:0;">
+        <el-header style="padding:0">
+          <!-- <i class="el-icon-caret-right" @click="pageActions"></i> -->
+          <el-card shadow="never">
+            <span>从不显示</span>
+            <el-card shadow="hover" body-style="padding:0" style="margin-top:-1%;width:7%;height:62px;float:right">
+              <el-button type="text" style="height:62px;width:60%" @click="popover"><span>姓名</span><i
+                  class="el-icon-arrow-down" style="color:red"></i></el-button>
+              <el-dialog title="提示" :visible.sync="centerDialogVisible" width="30%" style="border:5px red solid">
+                <span>需要注意的是内容是默认不居中的</span>
+                <span slot="footer" class="dialog-footer">
+                  <!-- <el-button @click="centerDialogVisible = false">取 消</el-button>
+                  <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button> -->
+                </span>
+              </el-dialog>
+            </el-card>
+          </el-card>
+        </el-header>
+        <el-main>
+
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
     </el-container>
   </div>
 </template>
@@ -50,6 +76,7 @@
 </script>
 <style>
   * {
+    width: 100%;
     margin: auto;
     padding: 0;
   }
@@ -64,7 +91,11 @@
   }
 
   .el-menu-vertical-demo:not(.el-menu--collapse) {
+    position: absolute;
+    left: 0;
+    top: 0;
     width: 200px;
-    min-height: 400px;
+    height: 100%;
+    /* min-height: 400px; */
   }
 </style>
