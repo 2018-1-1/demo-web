e05919b908626a0c798c605cce22e47a1a739380
Merlynr <lcq1013962426@gmail.com>
2019-01-05 22:19:53 +0800

    框架

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import homePage from '../view/home-page/home-page.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // },
     {
       path: '/home',
       name: "首页",
--- src/view/home-page/home-page.js
+++ src/view/home-page/home-page.js
@@ -5,7 +5,12 @@ export const homePage={
         }
     },
     methods: {
-        
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+          },
+          handleClose(key, keyPath) {
+            console.log(key, keyPath);
+          }
     },
     mounted() {
         
