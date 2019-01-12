69f3d9f3bd760da2abc7bdfb69a941b361301b56
Merlynr <lcq1013962426@gmail.com>
2019-01-12 17:58:19 +0800

    问卷分页

--- src/view/factor-analysis/factor-analysis.js
+++ src/view/factor-analysis/factor-analysis.js
@@ -1,10 +1,81 @@
-export const factorAnalysis={
-    data () {
-        return {
-            
-        }
-    },
-    methods: {
-        
+export const factorAnalysis = {
+  data() {
+    return {
+      currentPage: 5,
+      tableData3: [{
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-08',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-06',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-06',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-06',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-06',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-06',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }],
+      input21: '',
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: ''
     }
+  },
+  methods: {
+    searchStudent() {
+      console.log("dsa")
+      // 要进行过滤，必须改老师专业下的学生信息才可以显示
+    },
+    handleClick(row) {
+      console.log(row);
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
+  }
 }
--- src/view/factor-analysis/factor-analysis.vue
+++ src/view/factor-analysis/factor-analysis.vue
@@ -1,11 +1,40 @@
 <template>
-    <div>
+  <div>
+    <el-card shadow="hover" style="padding-bottom:20px">
+      <!-- TODO 搜索功能 -->
+      <el-select v-model="value" clearable placeholder="请选择问卷名称" style="display:inline-block;float:left;margin-left:20px">
+        <el-option v-for="(item,key) in options" :key="key" :label="item.label" :value="item.value">
+        </el-option>
+      </el-select>
+      <el-button type="primary" plain style="display:inline-block;float:left;margin-left:20px" @click="searchStudent">搜索</el-button>
+    </el-card>
+    <!-- 学生信息 -->
+    <el-table stripe :data="tableData3" border style="width: 100%;margin-top:1%">
+      <el-table-column prop="date" label="日期" width="180">
+      </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180">
+      </el-table-column>
+      <el-table-column prop="address" label="地址">
+      </el-table-column>
+      <el-table-column fixed="right" label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">个人信息</el-button>
+          <el-button @click="handleClick(scope.row)" type="text" size="small">人才培养计划</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div style="position:absolute;">
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage"
+        :page-sizes="[10, 20, 30, 50, 100]" :page-size="100" layout="total, sizes, prev, pager, next, jumper" :total="400">
+      </el-pagination>
     </div>
+  </div>
 </template>
 <script>
-import {factorAnalysis} from './factor-analysis.js'
-export default {
-    mixins:[factorAnalysis]
-}
+  import {
+    factorAnalysis
+  } from './factor-analysis.js'
+  export default {
+    mixins: [factorAnalysis]
+  }
 </script>
-
