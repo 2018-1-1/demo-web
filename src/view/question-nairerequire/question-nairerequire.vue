dea5a8b450e6d9e4863b5a0523362e6c4d813cb5
Merlynr <lcq1013962426@gmail.com>
2019-01-12 14:47:20 +0800

    页面适配1920*1080

--- src/view/question-nairerequire/question-nairerequire.vue
+++ src/view/question-nairerequire/question-nairerequire.vue
@@ -4,13 +4,13 @@
       <el-card>
         <h3 style="margin-left:-75px">{{questionnaire.name}}</h3>
         <el-form-item v-for="(item,key) in questionnaire.list" :key="key" :label="item.question">
-          <el-input v-if="item.type=='text'" style="width:700px;margin-left:-65%"></el-input>
+          <el-input v-if="item.type=='text'" style="width:700px;margin-left:-50%"></el-input>
           <el-radio-group v-if="item.type=='radio'">
             <el-radio v-for="(it,key) in item.questionnaire" :key="key" :label="it.value" style="width:50px;float:left;margin-left:50px">
             </el-radio>
           </el-radio-group>
           <el-checkbox-group v-if="item.type=='checkbox'">
-            <el-checkbox v-for="(it,key) in item.questionnaire" :key="key" :label="it.value" :name="type" style="width:50px;margin-left:50px;float:left"></el-checkbox>
+            <el-checkbox v-for="(it,key) in item.questionnaire" :key="key" :label="it.value" style="width:50px;margin-left:50px;float:left"></el-checkbox>
           </el-checkbox-group>
         </el-form-item>
         <el-form-item style="margin-left:-780px">
